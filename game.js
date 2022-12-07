c9cddc5e666f8139e4d9513e6259564b21cb0487
Fata Sefer <sefer.fata@gmail.com>
2022-12-07 10:40:23 +0100

    finish the logic

--- game.js
+++ game.js
@@ -11,39 +11,67 @@ const computerPlay = () => {
   return options[random];
 };
 
-const playRound = (playerSelection) => {
-  const computerSelection = computerPlay();
-
+const playRound = (playerSelection, computerSelection) => {
   if (playerSelection === computerSelection) {
-    return console.log("It's a draw!");
+    alert("It's a draw, nobody wins!");
+    return null;
   }
 
   switch (playerSelection) {
     case "rock":
       if (computerSelection === "scissors") {
-        console.log("You Win! Rock beats Scissors");
+        alert("You Win! Rock beats Scissors");
         return true;
       } else {
-        console.log("You Lose! Paper beats Rock");
+        alert("You Lose! Paper beats Rock");
         return false;
       }
 
     case "paper":
       if (computerSelection === "rock") {
-        console.log("You Win! Paper beats Rock");
+        alert("You Win! Paper beats Rock");
         return true;
       } else {
-        console.log("You Lose! Scissors beat Paper");
+        alert("You Lose! Scissors beat Paper");
         return false;
       }
 
     case "scissors":
       if (computerSelection === "paper") {
-        console.log("You Win! Scissors beat Paper");
+        alert("You Win! Scissors beat Paper");
         return true;
       } else {
-        console.log("You Lose! Rock beats Scissors");
+        alert("You Lose! Rock beats Scissors");
         return false;
       }
+  }
+};
+
+let userPoints = 0;
+let computerPoints = 0;
+let draws = 0;
+
+const game = (playerSelection) => {
+  const computerSelection = computerPlay();
+  const result = playRound(playerSelection, computerSelection);
+
+  if (result === true) {
+    userPoints = userPoints + 1;
+  } else if (result === false) {
+    computerPoints = computerPoints + 1;
+  } else {
+    draws = draws + 1;
+  }
+
+  if (userPoints === 5) {
+    userPoints = 0;
+    computerPoints = 0;
+    draws = 0;
+    return console.log("You won");
+  } else if (computerPoints === 5) {
+    userPoints = 0;
+    computerPoints = 0;
+    draws = 0;
+    return console.log("Sorry, Computer won!");
   }
 };
